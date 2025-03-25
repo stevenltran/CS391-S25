108d845235e4d0364b002f1bcfbfa9b211dd6c7f
stevenltran <steventranschool132@gmail.com>
2025-03-25 17:09:46 -0400

    chchanging the design

--- sparkbytes/src/pages/home.js
+++ sparkbytes/src/pages/home.js
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom";
 import "./home.css";
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-logo">SparkBytes!</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/login" className="navbar-button">Login</Link>
+        <Link to="profile">profile</Link>
+      </div>
+    </nav>
+  );
+}
+
 function Home() {
   return (
-    <div className="home-container">
-      <h1>Welcome to SparkBytes!</h1>
-      <p>Claim your food here.</p>
-      <Link to="/login" className="home-button">Get Started</Link>
-      <Link to="profile">profile</Link>
+    <div>
+      <Navbar />
+      <div className="home-container">
+        <h1>Welcome to SparkBytes!</h1>
+        <p>Claim your food here.</p>
+        <Link to="/login" className="home-button">Get Started</Link>
+      </div>
     </div>
   );
 }
